34417c6358a4e6745756960a2acad1363fb53e2a
Brian Welman <brianallisterwelman@gmail.com>
2026-02-25 09:24:18 +0200

    Added linting and formatting for TS

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,25 @@
-import typescriptEslint from "typescript-eslint";
+// @ts-check
 
-export default [{
-    files: ["**/*.ts"],
-}, {
-    plugins: {
-        "@typescript-eslint": typescriptEslint.plugin,
-    },
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
 
+export default defineConfig(
+  {
+    ignores: ['out/**'],
+  },
+  eslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    extends: [tseslint.configs.recommendedTypeChecked],
     languageOptions: {
-        parser: typescriptEslint.parser,
-        ecmaVersion: 2022,
-        sourceType: "module",
+      parserOptions: {
+        projectService: true,
+      },
     },
-
-    rules: {
-        "@typescript-eslint/naming-convention": ["warn", {
-            selector: "import",
-            format: ["camelCase", "PascalCase"],
-        }],
-
-        curly: "warn",
-        eqeqeq: "warn",
-        "no-throw-literal": "warn",
-        semi: "warn",
-    },
-}];
+  },
+  {
+    files: ['eslint.config.mjs'],
+    extends: [tseslint.configs.disableTypeChecked],
+  },
+);
